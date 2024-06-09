7a4295e3afd0a12451a2f2026a98b38ca8a56c6c
XavierSecond <xaversflex@gmail.com>
2024-06-09 00:43:15 -0400

    Fix scroll to top on refresh for all browsers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,51 @@
-import { About } from '@/components/About/About'
-import { Contact } from '@/components/Contact/Contact'
-import { Footer } from '@/components/Footer/Footer'
-import MobileNav from '@/components/MobileNav/MobileNav'
-import Nav from '@/components/Nav/Nav'
-import { Offers } from '@/components/Offers/Offers'
-import { Welcome } from '@/components/Welcome/Welcome'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { About } from '@/components/About/About';
+import { Contact } from '@/components/Contact/Contact';
+import { Footer } from '@/components/Footer/Footer';
+import MobileNav from '@/components/MobileNav/MobileNav';
+import Nav from '@/components/Nav/Nav';
+import { Offers } from '@/components/Offers/Offers';
+import { Welcome } from '@/components/Welcome/Welcome';
 
 const HomePage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
 
-  const [nav, setNav] = useState(false)
-  const openNav = () =>setNav(true)
-  const closeNav = () => setNav(false)
+  useEffect(() => {
+    // Scroll to the top when the component mounts
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100); // slight delay to ensure it works across all browsers
+  }, []);
+
   return (
-  <div className="overflow-x-hidden">
-    <div>
-      {/* Navbar */}
-      <Nav openNav={openNav}/>
-      <MobileNav nav={nav} closeNav={closeNav}/>
-      {/* Welcome */}
-      <Welcome />
-      {/* Services */}
-      <Offers />
-      {/* About */}
-      <About />
-      {/* Contact */}
-      <Contact />
-      {/* Footer */}
-      <Footer />
-
+    <div className="overflow-x-hidden">
+      <div>
+        {/* Navbar */}
+        <Nav openNav={openNav} />
+        <MobileNav nav={nav} closeNav={closeNav} />
+        {/* Welcome */}
+        <div id="home">
+          <Welcome />
+        </div>
+        {/* Services */}
+        <div id="services">
+          <Offers />
+        </div>
+        {/* About */}
+        <div id="about">
+          <About />
+        </div>
+        {/* Contact */}
+        <div id="contact">
+          <Contact />
+        </div>
+        {/* Footer */}
+        <Footer />
+      </div>
     </div>
-  </div>
-)}
+  );
+};
 
-export default HomePage
+export default HomePage;
